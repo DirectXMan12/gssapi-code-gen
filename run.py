f0e4b0a230368bd6c09658e13c6faa9e11836d57
Solly Ross <sross@redhat.com>
2015-03-30 17:19:41 -0400

    Allow running on specific methods from 'run.py'
    You can use 'run.py' to process specific methods by passing
    an argument of 'some.module.name#some_function_name'

--- run.py
+++ run.py
@@ -7,5 +7,22 @@ if __name__ == '__main__':
     import sys
 
     if len(sys.argv) == 2:
-        __import__(sys.argv[1])
-        print(gen.code_for_module(sys.modules[sys.argv[1]]))
+        raw_import_path = sys.argv[1]
+        if '#' in raw_import_path:
+            import_path, import_func = raw_import_path.split('#')
+        else:
+            import_path = raw_import_path
+            import_func = None
+
+        __import__(import_path)
+
+        module = sys.modules[import_path]
+        if import_func is not None:
+            func = getattr(module, import_func)
+            code = gen.code_for_function(func)
+        else:
+            code = gen.code_for_module(module)
+
+        print(code)
+    else:
+        sys.exit("You must supply a package or a method")
